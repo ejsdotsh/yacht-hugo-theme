2e8ca25571115fcac82312af0ded8c85508e912f
e.j. (they/them) <git@ejs.sh>
2026-09-12 16:23:26 -0700

    rewrite with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/*
+This file is present to satisfy a requirement of the Tailwind CSS IntelliSense
+extension for Visual Studio Code.
+
+https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+
+*/
